906a5d60f0827dfabdd9301033a755176201906c
kayo <kayohramos@gmail.com>
2022-09-27 16:44:14 -0300

    1-5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
       rare: '',
       cTrunfo: '',
       // hTrunfo: '',
-      // isSaveButtonDisabled: '',
+      isSaveButtonDisabled: true,
       // onInputChange: '',
       // onSaveButtonClick: '',
     };
@@ -26,7 +26,43 @@ class App extends React.Component {
     const value = type === 'checkbox' ? checked : event.target.value;
     this.setState({
       [name]: value,
-    });
+    }, this.bttDisabled);
+  };
+
+  bttDisabled = () => {
+    const { name, description, image, rare, attr1, attr2, attr3, isSaveButtonDisabled } = this.state;
+    // if ([name].includes('')) {
+    //   return false;  this.setState?
+    // }
+    const isName = (name.length !== 0);
+    const isDescription = (description.length !== 0);
+    const isImage = (image.length !== 0);
+    // const isRare = (rare.length !== 0);
+    const totalSum = 210;
+    const limit = 90;
+    const limitZero = 0;
+    const limit1 = (Number(attr1) >= limitZero);
+    const limit2 = (Number(attr2) >= limitZero);
+    const limit3 = (Number(attr3) >= limitZero);
+    const allLimit = limit1 && limit2 && limit3;
+    const limits1 = (Number(attr1) <= limit);
+    const limits2 = (Number(attr2) <= limit);
+    const limits3 = (Number(attr3) <= limit);
+    const limitsAll = limits1 && limits2 && limits3;
+    const result = Number(attr1) + Number(attr2) + Number(attr3);
+    const resultFinal = result <= totalSum;
+    // if (result <= totalSum) {
+    //   return result;
+    // }
+    // if (limits) {
+    //   return limits;
+    // }
+    const n = isName && isDescription && isImage && allLimit && limitsAll && resultFinal;
+    if (n) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
   };
 
   render() {
@@ -37,7 +73,8 @@ class App extends React.Component {
       attr3,
       image,
       rare,
-      cTrunfo } = this.state;
+      cTrunfo,
+      isSaveButtonDisabled } = this.state;
     return (
       <>
         <Form
@@ -50,6 +87,7 @@ class App extends React.Component {
           cardRare={ rare }
           cardTrunfo={ cTrunfo }
           onInputChange={ this.onInputChange }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
         />
         <Card
           cardName={ name }
